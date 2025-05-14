6af249c8d625da44a2f135de3ef59a0b8b93d06a
codersaradhi <chaitanyasaradhi1999@gmail.com>
2025-05-14 14:43:01 +0530

    updated the routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,36 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Body from "./Body";
 import Login from "./Login";
 import Profile from "./Profile";
 
-function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Body />}>
-            <Route path="login" element={<Login />} />
-            <Route path="profile" element={<Profile />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </>
-  );
-}
 
-export default App;
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<Body/>,
+    children:[
+      {path:"login",element:<Login/>},
+      {path:"profile",element:<Profile/>}
+    ],
+  },
+
+]);
+
+// function App() {
+//   return (
+//     <>
+//       <BrowserRouter>
+//         <Routes>
+//           <Route path="/" element={<Body />}>
+//             <Route path="login" element={<Login />} />
+//             <Route path="profile" element={<Profile />} />
+//           </Route>
+//         </Routes>
+//       </BrowserRouter>
+//     </>
+//   );
+// }
+
+export default function App(){
+  return <RouterProvider router={router}/>
+};
